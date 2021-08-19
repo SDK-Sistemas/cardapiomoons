50d8e2767f63b41e137f7b6c9490e47dd918ced6
adrianonetto <adriano.netto2010@gmail.com>
2021-08-19 11:03:52 -0300

    newslatter

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,12 +1,16 @@
 <div class="subscribe_section">
     <div class="wrap">
         <div class="wrap_float">
-            <div class="subscribe_block" style="background-image: url(img/demo-bg.jpg)">
+            <div class="subscribe_block"
+                style="background-image: url(https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80)">
                 <div class="left">
                     <h2 class="_title">Newslatters</h2>
                     <p class="_subtitle">Sign up to receive the best offers</p>
                 </div>
                 <div class="right">
+                    <div class="input_wrap" style="margin: 0px 16px 16px 0px">
+                        <input type="text" class="input" placeholder="Nome">
+                    </div>
                     <div class="input_wrap">
                         <input type="email" class="input" placeholder="Email">
                     </div>
