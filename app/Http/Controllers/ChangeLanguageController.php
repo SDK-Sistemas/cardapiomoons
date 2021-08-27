523a7b9630b7075083d2dd5bf251b976d7e51753
adrianonetto <adriano.netto2010@gmail.com>
2021-08-27 17:54:56 -0300

    change language controller

---
+++ app/Http/Controllers/ChangeLanguageController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+/**
+ * Change the locale
+ */
+class ChangeLanguageController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function __invoke(Request $request): RedirectResponse
+    {
+        Session::put('lang', $request->input('lang') );
+        return back();
+    }
+}
