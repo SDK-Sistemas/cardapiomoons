3c559b33951bbd1bd2c5c0f7e9ab3741ed30c28e
SDK Sistemas <76075685+SDKSistemas@users.noreply.github.com>
2021-10-25 16:33:18 -0300

    Ajuste Page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>Trip in</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Prata&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
